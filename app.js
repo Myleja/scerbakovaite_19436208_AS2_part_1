555d126c2659031b4f7462cb6b638dfccc17e47f
Myleja <56394362+Myleja@users.noreply.github.com>
2020-08-06 18:14:34 +0100

    06/08/2020

---
+++ app.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  anime({
+    targets: ".logo",
+    translateX: [-400, 0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    opacity: [0, 1],
+    delay: 1000,
+  });
+
+  anime({
+    targets: "a",
+    translateY: [-40, 0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    opacity: [0, 1],
+    delay: function (el, i, l) {
+      return 2000 + 100 * i;
+    },
+  });
+
+  anime({
+    targets: "h1",
+    translateX: [-400, 0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    opacity: [0, 1],
+    delay: 1200,
+  });
+
+  anime({
+    targets: "h2,p",
+    translateY: [-400, 0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    opacity: [0, 1],
+    delay: 1500,
+  });
+
+  anime({
+    targets: "img",
+    translateX: [-400, 0],
+    easing: "easeOutExpo",
+    duration: 1500,
+    opacity: [0, 1],
+    delay: 1500,
+  });
+});
