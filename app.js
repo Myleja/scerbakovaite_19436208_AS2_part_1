bdfd4a5c6cc4a98bdb4583c7dfb8b3b479ba5d68
Myleja <56394362+Myleja@users.noreply.github.com>
2020-08-08 20:11:49 +0100

    08/08/2020

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-document.addEventListener("DOMContentLoaded", () => {
-  anime({
-    targets: ".logo",
-    translateX: [-400, 0],
-    easing: "easeOutExpo",
-    duration: 1500,
-    opacity: [0, 1],
-    delay: 1000,
-  });
+const chk = document.getElementById('chk');
 
-  anime({
-    targets: "a",
-    translateY: [-40, 0],
-    easing: "easeOutExpo",
-    duration: 1500,
-    opacity: [0, 1],
-    delay: function (el, i, l) {
-      return 2000 + 100 * i;
-    },
-  });
-
-  anime({
-    targets: "h1",
-    translateX: [-400, 0],
-    easing: "easeOutExpo",
-    duration: 1500,
-    opacity: [0, 1],
-    delay: 1200,
-  });
-
-  anime({
-    targets: "h2,p",
-    translateY: [-400, 0],
-    easing: "easeOutExpo",
-    duration: 1500,
-    opacity: [0, 1],
-    delay: 1500,
-  });
-
-  anime({
-    targets: "img",
-    translateX: [-400, 0],
-    easing: "easeOutExpo",
-    duration: 1500,
-    opacity: [0, 1],
-    delay: 1500,
-  });
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   anime({
+//     targets: ".logo",
+//     translateX: [-400, 0],
+//     easing: "easeOutExpo",
+//     duration: 1500,
+//     opacity: [0, 1],
+//     delay: 1000,
+//   });
+
+//   anime({
+//     targets: "a",
+//     translateY: [-40, 0],
+//     easing: "easeOutExpo",
+//     duration: 1500,
+//     opacity: [0, 1],
+//     delay: function (el, i, l) {
+//       return 2000 + 100 * i;
+//     },
+//   });
+
+//   anime({
+//     targets: "h1",
+//     translateX: [-400, 0],
+//     easing: "easeOutExpo",
+//     duration: 1500,
+//     opacity: [0, 1],
+//     delay: 1200,
+//   });
+
+//   anime({
+//     targets: "h2,p",
+//     translateY: [-400, 0],
+//     easing: "easeOutExpo",
+//     duration: 1500,
+//     opacity: [0, 1],
+//     delay: 1500,
+//   });
+
+//   anime({
+//     targets: "img",
+//     translateX: [-400, 0],
+//     easing: "easeOutExpo",
+//     duration: 1500,
+//     opacity: [0, 1],
+//     delay: 1500,
+//   });
+// });
