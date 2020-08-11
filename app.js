320efd40551126a808c7e25a27209784c11c775e
Myleja <56394362+Myleja@users.noreply.github.com>
2020-08-11 17:59:15 +0100

    11/08/2020

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-// SELECTORS
-const darkModeTgl = document.getElementById('toggle');
-
-// EVENT LISTENERS
-//event listener for dark mode toggle
-darkModeTgl.addEventListener('change', dark);
-
-
-//FUNCTIONS
+// selector for dark mode toggle
+const darkModeTgl = document.getElementById("toggle");
+// add event listener for dark mode toggle
+darkModeTgl.addEventListener("change", dark);
 //function for dark mode
 function dark() {
-  document.body.classList.toggle('dark');
+  document.body.classList.toggle("dark");
+}
+//selector for text animation
+const text = document.querySelector(".intro-text")
+// split text in to characters
+const strText = text.textContent;
+const splitText = strText.split("");
+//remove h1 after span created
+text.textContent = "";
+// create a span
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+//create interval
+let char = 0;
+let timer = setInterval(onTick, 50);
+// function for text animation
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  //when the characters finished, stop runinng
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+function complete() {
+  clearInterval(timer);
+  timer = null;
 }
 
 // document.addEventListener("DOMContentLoaded", () => {
