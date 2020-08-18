dcef12e5d926c513ed6db33d2cf72dbd8be02b59
Myleja <56394362+Myleja@users.noreply.github.com>
2020-08-18 07:53:15 +0100

    18/08/2020

--- app.js
+++ app.js
@@ -31,51 +31,55 @@ darkModeTgl.addEventListener("click", () => {
   }
 });
 
-//selector for text animation
-// const text = document.querySelector(".intro-text");
-// // split text in to characters
-// const strText = text.textContent;
-// const splitText = strText.split("");
-// //remove h1 after span created
-// text.textContent = "";
-// // create a span
-// for (let i = 0; i < splitText.length; i++) {
-//   text.innerHTML += "<span>" + splitText[i] + "</span>";
-// }
-// //create interval
-// let char = 0;
-// let timer = setInterval(onTick, 50);
-// // function for text animation
-// function onTick() {
-//   const span = text.querySelectorAll("span")[char];
-//   span.classList.add("fade");
-//   char++;
-//   //when the characters finished, stop runinng
-//   if (char === splitText.length) {
-//     complete();
-//     return;
-//   }
-// }
-// function complete() {
-//   clearInterval(timer);
-//   timer = null;
-// }
-
 const nav = document.querySelector(".links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
-  burger.classList.toggle("toggle");
+  burger.classList.toggle("nav-close");
 });
 
 links.forEach(link => {
   link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
-    burger.classList.toggle("toggle");
+    burger.classList.toggle("nav-close");
   });
 });
+
+
+
+//selector for text animation
+const text = document.querySelector(".intro-text");
+// split text in to characters
+const strText = text.textContent;
+const splitText = strText.split("");
+//remove h1 after span created
+text.textContent = "";
+// create a span
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+//create interval
+let char = 0;
+let timer = setInterval(onTick, 50);
+// function for text animation
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  //when the characters finished, stop runinng
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
+
 
 // document.addEventListener("DOMContentLoaded", () => {
 //   anime({
